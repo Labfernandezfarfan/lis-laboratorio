da5b6696ce8d5d64c9df84ed67f2087c4e11d103
Labfernandezfarfan <lab.fernandezfarfan@gmail.com>
2026-07-27 20:00:53 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -342,7 +342,7 @@ def conectar_db():
     return psycopg2.connect(
         host="aws-1-us-west-2.pooler.supabase.com",
         port=5432,
-        user="postgres.hgukbkxetzlxossfvif",
+        user="postgres.hgukbkxetzxlxossfvif",
         password="Laboratorio135Lis", # Pon tu contraseña real aquí, asegúrate de que no tenga símbolos raros
         dbname="postgres",
         sslmode="require"
